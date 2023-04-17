515aaa5ce2e9a6549a063eb418a543b267839ae5
Vichea <nathvichea1@gmail.com>
2023-04-17 11:33:02 +0700

    Add miu simple

--- run.sh
+++ run.sh
@@ -3,10 +3,15 @@ file=$1
 
 # remove extension
 file=${file%.*}
+# remove old .out file
+rm -f $file.out
 
 g++ -std=c++11 -O2 -Wall $file.cpp -o $file.out
-echo "Compiled Successfully!..."
-echo "Running $file.out"
-echo "Output: "
 
-./$file.out
+# echo with color
+echo -e "\033[0;32mCompiling $file.cpp\033[0m"
+echo -e "\033[0;32mRunning $file.out\033[0m"
+echo -e "\033[0;32mOutput:\033[0m"
+
+# try catch ./$file.out runtime error
+./$file.out || echo -e "\033[0;31mRuntime Error\033[0m"
